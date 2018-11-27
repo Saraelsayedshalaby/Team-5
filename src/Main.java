17e69434d754bbd082b3579937f8f1ed8f230cbe
Saraelsayedshalaby <45360299+Saraelsayedshalaby@users.noreply.github.com>
2018-11-27 08:15:45 +0200

    add palandrome
    when i add it not boaleon so i edit it

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
-
+public class Main
+{
+	public static boolean isPalindrome(String inputString){
+    int length  = inputString.length();
+    int i, begin, end, middle;
+    begin  = 0;
+    end    = length - 1;
+    middle = (begin + end)/2;
+		for (i = begin; i <= middle; i++) {
+			if (inputString.charAt(begin) == inputString.charAt(end)) {
+			   begin++;
+			   end--;
+			}else {
+				break;
+			}
+		}
+		if (i == middle + 1) {
+		  return true;
+		}else {
+		  return false;\\not palaindrome
+	    }  
+	}
+	
 
 public class Main
 {
